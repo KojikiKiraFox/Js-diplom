ecba298b730e9f7ccf75d256f888bccbf2e40919
Fox <90596095+KojikiKiraFox@users.noreply.github.com>
2022-05-05 13:41:00 +0300

    small commit

--- src/modules/repair.js
+++ src/modules/repair.js
@@ -1,11 +1,49 @@
 const repair = () => {
+    const navList = document.querySelector('.nav-list-repair')
+    const repairNavs = document.querySelectorAll('.repair-types-nav__item')
+    const arrowRight = document.querySelector('#repair-types-arrow_right')
+    const arrowLeft = document.querySelector('#repair-types-arrow_left')
 
-    const itemActive = document.querySelectorAll('.repair-types-nav__item')
+    const slideCount = document.querySelector('.slider-counter-content__current')
+    const slideCoutTotal = document.querySelector('.slider-counter-content__total')
 
-    itemActive.forEach(function (item) {
-        item.addEventListener('click', function() {
-            this.classList.toggle('active')
+    const slideItem = document.querySelectorAll('.types-repair1 > .repair-types-slider__slide')
+
+    let count = 0
+
+    slideCoutTotal.textContent = slideItem.length
+    const prevSlide = (index, strClass) => {
+        repairNavs[index].classList.remove(strClass)
+    }
+
+    const nextSlide = (index, strClass) => {
+        repairNavs[index].classList.add(strClass)
+    }
+
+    repairNavs.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault()
+            const elem = navList.querySelector('.active')
+            elem.classList.remove('active')
+            e.target.classList.add('active')
         })
+    })
+    
+    arrowRight.addEventListener('click', () => {
+        slideCount.textContent = count + 1
+        count++
+        if (count == slideCoutTotal.textContent) {
+            count = 0
+        }
+    })
+
+    arrowLeft.addEventListener('click', () => {
+
+        count--
+        if (count == -1) {
+            count = slideCount.textContent
+        }
+
         
     })
 }
